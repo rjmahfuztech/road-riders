75d43bdc9e700c845df96f75724744791e1c3bce
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-04-18 21:42:32 +0600

    added

--- src/components/Admin/OrderList/OrderList.js
+++ src/components/Admin/OrderList/OrderList.js
@@ -9,53 +9,55 @@ const OrderList = () => {
             .then(res => res.json())
             .then(data => setAllOrders(data));
     }, []);
-    
+
     return (
-        <div className="margin container">
-            <div className="row">
-                <div className="col-md-2">
-                    <Sidebar></Sidebar>
-                </div>
-                <div className="col-md-10">
-                    <h2 className="mb-5">Order List</h2>
-                    <div>
-                        {
-                            allOrders.length ?
-                                <table className="table">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Email</th>
-                                            <th scope="col">Service</th>
-                                            <th scope="col">Payment Id</th>
-                                            <th scope="col">Status</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {
-                                            allOrders.map(orders =>
-                                                <tr key={orders._id}>
-                                                    <td>{orders.name}</td>
-                                                    <td>{orders.email}</td>
-                                                    <td>{orders.OrderInfo?.ServiceName}</td>
-                                                    <td>{orders.paymentId}</td>
-                                                    <td>
-                                                        <select className="form-select" aria-label="Default select example">
-                                                            <option value="Pending">Pending</option>
-                                                            <option value="On going">On going</option>
-                                                            <option value="Done">Done</option>
-                                                        </select>
-                                                    </td>
-                                                </tr>
-                                            )
-                                        }
-                                    </tbody>
-                                </table>
-                                :
-                                <div className="text-center text-secondary">
-                                    <h4>No orders yet</h4>
-                                </div>
-                        }
+        <div className="container">
+            <div className="margin">
+                <div className="row">
+                    <div className="col-md-2">
+                        <Sidebar></Sidebar>
+                    </div>
+                    <div className="col-md-10">
+                        <h2 className="mb-5">Order List</h2>
+                        <div>
+                            {
+                                allOrders.length ?
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Service</th>
+                                                <th scope="col">Payment Id</th>
+                                                <th scope="col">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                allOrders.map(orders =>
+                                                    <tr key={orders._id}>
+                                                        <td>{orders.name}</td>
+                                                        <td>{orders.email}</td>
+                                                        <td>{orders.OrderInfo?.ServiceName}</td>
+                                                        <td>{orders.paymentId}</td>
+                                                        <td>
+                                                            <select className="form-select" aria-label="Default select example">
+                                                                <option value="Pending">Pending</option>
+                                                                <option value="On going">On going</option>
+                                                                <option value="Done">Done</option>
+                                                            </select>
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            }
+                                        </tbody>
+                                    </table>
+                                    :
+                                    <div className="text-center text-secondary">
+                                        <h4>No orders yet</h4>
+                                    </div>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
