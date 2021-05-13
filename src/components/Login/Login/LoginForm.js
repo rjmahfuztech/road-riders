c4b15e87f16b8ae94fa24a7e8a0af40c8e5249c3
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-05-13 23:21:33 +0600

    added

--- src/components/Login/Login/LoginForm.js
+++ src/components/Login/Login/LoginForm.js
@@ -7,45 +7,45 @@ import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 const LoginForm = ({ handleBlur, handleSubmit, user, handleGoogleSignIn, newUser, setNewUser }) => {
 
     return (
-        <Zoom>
         <div className="login-form-style">
-            <h3 className="text-center mb-4">
-                {newUser ? 'Create an account' : 'Sign In'}
-            </h3>
-            <form onSubmit={handleSubmit}>
-                {
-                    newUser &&
+            <Zoom>
+                <h3 className="text-center mb-4">
+                    {newUser ? 'Create an account' : 'Sign In'}
+                </h3>
+                <form onSubmit={handleSubmit}>
+                    {
+                        newUser &&
+                        <div className="mb-3">
+                            <label className="form-label">Your Name</label>
+                            <input type="text" name="name" onBlur={handleBlur} className="form-control" placeholder="Enter your name" required />
+                        </div>
+                    }
                     <div className="mb-3">
-                        <label className="form-label">Your Name</label>
-                        <input type="text" name="name" onBlur={handleBlur} className="form-control" placeholder="Enter your name" required />
+                        <label className="form-label">Email address</label>
+                        <input type="email" name="email" onBlur={handleBlur} className="form-control" placeholder="Enter your email" required />
                     </div>
-                }
-                <div className="mb-3">
-                    <label className="form-label">Email address</label>
-                    <input type="email" name="email" onBlur={handleBlur} className="form-control" placeholder="Enter your email" required />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Password</label>
-                    <input type="password" name="password" onBlur={handleBlur} className="form-control" placeholder="Enter your password" required />
-                </div>
-                <button type="submit" className="form-control btn btn-primary">{newUser ? 'Sign Up' : 'Login'}</button>
-            </form>
-            <p className="mt-3 mb-2 text-danger">{user.error}</p>
-            {
-                user.success && <p className="mt-3 mb-2 text-success">{newUser ? 'Account created' : 'User Logged In'} successfully</p>
-            }
-            <div className="mt-3 text-right">
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input type="password" name="password" onBlur={handleBlur} className="form-control" placeholder="Enter your password" required />
+                    </div>
+                    <button type="submit" className="btn btn-primary form-control">{newUser ? 'Sign Up' : 'Login'}</button>
+                </form>
+                <p className="mt-3 mb-2 text-danger">{user.error}</p>
                 {
-                    newUser ? <p>Already have an account ? <span className="toggle-sign-in-login" onClick={() => setNewUser(!newUser)}>Login</span></p> :
-                        <p>Don't have an account ? <span className="toggle-sign-in-login" onClick={() => setNewUser(!newUser)}>Sign Up</span></p>
+                    user.success && <p className="mt-3 mb-2 text-success">{newUser ? 'Account created' : 'User Logged In'} successfully</p>
                 }
-            </div>
-            <div>
-                <p className="text-center mt-2 mb-3 or">OR</p>
-                <button onClick={handleGoogleSignIn} className="form-control btn btn-outline-primary"><FontAwesomeIcon icon={faGooglePlusG} /> Continue with Google</button>
-            </div>
+                <div className="mt-3 text-right">
+                    {
+                        newUser ? <p>Already have an account ? <span className="toggle-sign-in-login" onClick={() => setNewUser(!newUser)}>Login</span></p> :
+                            <p>Don't have an account ? <span className="toggle-sign-in-login" onClick={() => setNewUser(!newUser)}>Sign Up</span></p>
+                    }
+                </div>
+                <div>
+                    <p className="text-center mt-2 mb-3 or">OR</p>
+                    <button type="button" onClick={handleGoogleSignIn} className="btn btn-outline-primary form-control"><FontAwesomeIcon icon={faGooglePlusG} /> Continue with Google</button>
+                </div>
+            </Zoom>
         </div>
-        </Zoom>
     );
 };
 
