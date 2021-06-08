de941408f85d6975c4048757f29c9b95f1970f71
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-06-09 00:35:00 +0600

    some changes

--- src/components/Home/NavigationBar/NavigationBar.js
+++ src/components/Home/NavigationBar/NavigationBar.js
@@ -1,38 +1,61 @@
-import React, { useContext } from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../../../App';
-import { signOut } from '../../Login/Login/LoginManager';
-import './NavigationBar.css';
+import React, {useContext} from "react";
+import {Nav, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {UserContext} from "../../../App";
+import {signOut} from "../../Login/Login/LoginManager";
+import "./NavigationBar.css";
 
 const NavigationBar = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
-    const handleSingOut = () => {
-        signOut();
-        setLoggedInUser('');
-    }
-    return (
-        <div>
-            <Navbar variant="dark" expand="md">
-                <Navbar.Brand as={Link} to="/">ROAD RIDERS</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                    <Nav>
-                        <Nav.Link className="nav-style" as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link className="nav-style" href="#blog">Blog</Nav.Link>
-                        <Nav.Link className="nav-style" href="#service">Service</Nav.Link>
-                        <Nav.Link className="nav-style" as={Link} to="/admin">Admin</Nav.Link>
-                        <Nav.Link className="nav-style" href="#contact">Contact Us</Nav.Link>
-                        {
-                            loggedInUser.email ? <Nav.Link onClick={handleSingOut} className="nav-style nav-end" as={Link} to="/">Sign Out</Nav.Link>
-                            : <Nav.Link className="nav-style nav-end" as={Link} to="/login">Login</Nav.Link>
-                        }
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
+  const handleSingOut = () => {
+    signOut();
+    setLoggedInUser("");
+  };
+  return (
+    <Navbar collapseOnSelect variant="dark" expand="md">
+      <Navbar.Brand as={Link} to="/">
+        ROAD RIDERS
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        className="justify-content-end"
+        id="responsive-navbar-nav"
+      >
+        <Nav>
+          <Nav.Link className="nav-style" as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link className="nav-style" href="#blog">
+            Blog
+          </Nav.Link>
+          <Nav.Link className="nav-style" href="#service">
+            Service
+          </Nav.Link>
+          <Nav.Link className="nav-style" as={Link} to="/admin">
+            Admin
+          </Nav.Link>
+          <Nav.Link className="nav-style" href="#contact">
+            Contact Us
+          </Nav.Link>
+          {loggedInUser.email ? (
+            <Nav.Link
+              onClick={handleSingOut}
+              className="nav-style nav-end"
+              as={Link}
+              to="/"
+            >
+              Sign Out
+            </Nav.Link>
+          ) : (
+            <Nav.Link className="nav-style nav-end" as={Link} to="/login">
+              Login
+            </Nav.Link>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
